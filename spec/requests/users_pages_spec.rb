2afc67a95470e87209b4e7471ca68d93765b53ae
ormus2002 <ormus2002@gmail.com>
2013-04-03 19:26:23 +0300

    Finish user signup

--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -22,4 +22,39 @@ describe "User pages" do
     it { should have_selector('h1',    text: 'Регистрация') }
     it { should have_selector('title', text: full_title('Регистрация')) }
   end
+  
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+  
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Зарегистрироваться" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Имя",            with: "Example User"
+        fill_in "Е-мейл",         with: "user@example.com"
+        fill_in "Пароль",         with: "foobar"
+        fill_in "Подтверждение",  with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+  
 end
